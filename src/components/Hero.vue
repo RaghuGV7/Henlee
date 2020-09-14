62981564843dffdc4021337ae10e332c834c7158
Raghu Gattapur <rgattapur@gpstrategies.com>
2020-09-14 22:01:09 +0100

    updated hero display

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
     <div class="jumbotron jumbotron-fluid home-hero">
       <div class="container">
         <h1 class="d-none d-md-block display-3">Experts in ERP implementations, Data migration and Business Intelligence</h1>
-        <h1 class="d-none d-sm-block">Experts in ERP implementations, Data migration and Business Intelligence</h1>
+        <h1 class="d-md-none">Experts in ERP implementations, Data migration and Business Intelligence</h1>
         <p class="lead p-2">We migrate, maintain and transform our data.</p>
         <b-button class="m-2" variant="primary" href="#services">Our Services</b-button>
       </div>
