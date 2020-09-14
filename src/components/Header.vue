a8ee31ea2a816aa96144db2d4176c168a0300e89
Raghu Gattapur <rgattapur@gpstrategies.com>
2020-09-14 22:13:18 +0100

    final changes for today.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <b-navbar toggleable="lg" fixed="top" type="light" class="navbar-bg">
-    <div class="top-logo"></div>
+    <div class="top-logo d-none d-md-block"></div>
+    <div class="top-logo top-logo-sm d-md-none d-block"></div>
     <b-navbar-brand to="/" class="p-0">
       <h2 class="brand-title">Henlee Ltd</h2>
       <!-- <small class="brand-caption">The Data Service Provider</small> -->
@@ -11,28 +12,26 @@
     <b-collapse id="nav-collapse" is-nav>
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#home" right>Home</b-nav-item>
-          <b-nav-item-dropdown right>
-            <template v-slot:button-content>Services</template>
-            <b-dropdown-item href="#services"><i class="fa fa-angle-double-up"></i> Business Process Improvement</b-dropdown-item>
-            <b-dropdown-item href="#"><i class="fa fa-wrench"></i> Change Management</b-dropdown-item>
-            <b-dropdown-item href="#"><i class="fa fa-cogs"></i> ERP Implementations</b-dropdown-item>
-            <b-dropdown-item href="#"><i class="fa fa-chart-line"></i> Data Management & Business Intelligence</b-dropdown-item>
-            <b-dropdown-item href="#"><i class="fa fa-sync"></i> Data Integrations</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item-dropdown right>
-            <template v-slot:button-content>Technologies</template>
-            <b-dropdown-item href="#">Microsoft Dynamics AX 2012</b-dropdown-item>
-            <b-dropdown-item href="#">Microsoft Dynamics 365 F&O</b-dropdown-item>
-            <b-dropdown-item href="#">Microsoft Dynamics 365 Sales</b-dropdown-item>
-            <b-dropdown-item href="#">Microsoft Dynamics 365 Field Service</b-dropdown-item>
-            <b-dropdown-item href="#">Power BI</b-dropdown-item>
-            <b-dropdown-item href="#">SQL Services</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item right href="#contact">Contact</b-nav-item>
-          <b-nav-item right href="#about">About Us</b-nav-item>
-        </b-navbar-nav>
+        <b-nav-item href="#home" right>Home</b-nav-item>
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>Services</template>
+          <b-dropdown-item href="#services"><i class="fa fa-angle-double-up"></i> Business Process Improvement</b-dropdown-item>
+          <b-dropdown-item href="#"><i class="fa fa-wrench"></i> Change Management</b-dropdown-item>
+          <b-dropdown-item href="#"><i class="fa fa-cogs"></i> ERP Implementations</b-dropdown-item>
+          <b-dropdown-item href="#"><i class="fa fa-chart-line"></i> Data Management & Business Intelligence</b-dropdown-item>
+          <b-dropdown-item href="#"><i class="fa fa-sync"></i> Data Integrations</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>Technologies</template>
+          <b-dropdown-item href="#">Microsoft Dynamics AX 2012</b-dropdown-item>
+          <b-dropdown-item href="#">Microsoft Dynamics 365 F&O</b-dropdown-item>
+          <b-dropdown-item href="#">Microsoft Dynamics 365 Sales</b-dropdown-item>
+          <b-dropdown-item href="#">Microsoft Dynamics 365 Field Service</b-dropdown-item>
+          <b-dropdown-item href="#">Power BI</b-dropdown-item>
+          <b-dropdown-item href="#">SQL Services</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item right href="#contact">Contact</b-nav-item>
+        <b-nav-item right href="#about">About Us</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
     <!-- <div class="container">
